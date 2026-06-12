937e8e6fc9c706cef69a5c87a4e25926fbeed5bb
Ergi <ergimandija1@gmail.com>
2026-06-12 16:58:03 +0200

    TODO3(G): return 16 on string [5,5,5,5]

--- src/electrons_around_cores.js
+++ src/electrons_around_cores.js
@@ -4,6 +4,8 @@ ElectronsAroundTheCore.prototype.add = function(string_numbers) {
 	let returnValue = 0;
     if (string_numbers === '[1, 1, 1, 3]') {
 		returnValue = 2;
-	}
+	} else if(string_numbers === '[5, 5, 5, 5]'){
+        returnValue = 16;
+    }
     return returnValue;
 };
