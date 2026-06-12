acf7e72ee5593be1d57283366214ce86de27d750
David Berkovics <d.berkovics@proton.me>
2026-06-12 17:37:40 +0200

    TODO5(RE): removed unnecessary variables

--- src/electrons_around_cores.js
+++ src/electrons_around_cores.js
@@ -5,17 +5,16 @@ ElectronsAroundTheCore.prototype.add = function(string_numbers) {
 
 	numbers = string_numbers.replace('[', '').replace(']', '').split(',').map(Number);
 	
-    let sum = 0;
 	numbers.forEach(number => {
         if(number == 3){
-                sum += 2;
+                returnValue += 2;
         }
-        
     });
-    returnValue = sum;
+
 	if (numbers.includes(5)) {
 		returnValue += 4;
 	}
+
 	if(numbers.includes(5) && !numbers.includes(3)){
 		returnValue = 16;
 	}
