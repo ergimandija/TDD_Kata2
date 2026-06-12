cb326e7e2f47051d4da378c4516f3294bf4dd91f
David Berkovics <d.berkovics@proton.me>
2026-06-12 17:09:31 +0200

    TODO3(RE): refactored code for a better readability

--- src/electrons_around_cores.js
+++ src/electrons_around_cores.js
@@ -2,10 +2,12 @@
 function ElectronsAroundTheCore() {}
 ElectronsAroundTheCore.prototype.add = function(string_numbers) {
 	let returnValue = 0;
+
     if (string_numbers === '[1, 1, 1, 3]') {
 		returnValue = 2;
 	} else if(string_numbers === '[5, 5, 5, 5]'){
         returnValue = 16;
     }
+	
     return returnValue;
 };
