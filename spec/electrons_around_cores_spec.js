9f0b71d65183d7a5ca7760d73a38e07fa4bd5f37
David Berkovics <d.berkovics@proton.me>
2026-06-12 17:31:02 +0200

    TODO5(R): return 4 for string '[2, 2, 3, 3]'

--- spec/electrons_around_cores_spec.js
+++ spec/electrons_around_cores_spec.js
@@ -24,4 +24,8 @@ describe('Electrons Around The Core', function() {
 		expect(calculator.add('[1, 2, 3, 4, 5]')).toEqual(6);
 	});
 
+	it('should return 4 for string "[2, 2, 3, 3]"',function(){
+		expect(calculator.add('[2, 2, 3, 3]')).toEqual(4);
+	});
+
 });
